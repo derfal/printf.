d571cc2ae7aedc3942da1ec54a49f9c58a6bde7e
parksomin72 <fatimazahraealawi@gmail.com>
2023-07-26 16:33:51 -0700

    This is a printf files

--- main.c
+++ main.c
@@ -38,5 +38,7 @@ _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
 _printf("Unknown:[%r]\n");
 printf("Unknown:[%r]\n");
+_printf("%.6s", "Best School !\n");
+_printf("%.*s", 6, "Best School !\n");
 return (0);
 }
